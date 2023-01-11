6703155274fc8db133d30eae440dd0b9db830337
ricardo <richygp@gmail.com>
2023-01-11 22:17:06 +0100

    First commit

---
+++ src/main/java/com/ryanair/finterconnection/FinterconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.ryanair.finterconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinterconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinterconnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanair/finterconnection/FinterconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanair.finterconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinterconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
