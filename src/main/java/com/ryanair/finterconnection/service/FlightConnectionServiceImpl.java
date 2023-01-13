a31586f2450374b6a1df46daee62c70d375b1844
ricardo <richygp@gmail.com>
2023-01-14 00:16:37 +0100

    Refactors interconnection service

--- src/main/java/com/ryanair/finterconnection/service/FlightConnectionServiceImpl.java
+++ src/main/java/com/ryanair/finterconnection/service/FlightConnectionServiceImpl.java
@@ -4,7 +4,6 @@ import com.ryanair.finterconnection.apiclients.ryanairroutesclient.IRyanairRoute
 import com.ryanair.finterconnection.apiclients.ryanairscheduleclient.IRyanairScheduleApiClient;
 import com.ryanair.finterconnection.domain.Leg;
 import com.ryanair.finterconnection.domain.Route;
-import com.ryanair.finterconnection.dto.DayDTO;
 import com.ryanair.finterconnection.dto.ScheduleDTO;
 import org.springframework.stereotype.Service;
 
@@ -27,26 +26,24 @@ public class FlightConnectionServiceImpl implements IFlightConnectionService {
     public List<Leg> getDirectConnections(String departure, String arrival,
                                           LocalDateTime departureDateTime, LocalDateTime arrivalDateTime) {
         ScheduleDTO schedule = scheduleApiClient.getScheduleForYearAndMonth(departure, arrival, departureDateTime);
-        List<DayDTO> days = schedule.days().stream()
-                .filter(d -> d.day() >= departureDateTime.getDayOfMonth() && d.day() <= arrivalDateTime.getDayOfMonth())
-                .toList();
 
-        return days.stream()
+        return schedule.days().stream()
+                .filter(d -> d.day() >= departureDateTime.getDayOfMonth() && d.day() <= arrivalDateTime.getDayOfMonth())
                 .flatMap(d -> d.flights().stream()
                         .map((f -> new Leg(
-                                departure, arrival,
-                                LocalDateTime.of(
-                                        departureDateTime.getYear(),
-                                        departureDateTime.getMonth(),
-                                        d.day(),
-                                        f.departureTime().getHour(),
-                                        f.departureTime().getMinute()),
-                                LocalDateTime.of(
-                                        arrivalDateTime.getYear(),
-                                        arrivalDateTime.getMonth(),
-                                        d.day(),
-                                        f.arrivalTime().getHour(),
-                                        f.arrivalTime().getMinute()))
+                                        departure, arrival,
+                                        LocalDateTime.of(
+                                                departureDateTime.getYear(),
+                                                departureDateTime.getMonth(),
+                                                d.day(),
+                                                f.departureTime().getHour(),
+                                                f.departureTime().getMinute()),
+                                        LocalDateTime.of(
+                                                arrivalDateTime.getYear(),
+                                                arrivalDateTime.getMonth(),
+                                                d.day(),
+                                                f.arrivalTime().getHour(),
+                                                f.arrivalTime().getMinute()))
                                 )
                         ).filter(l ->
                                 l.departureDateTime().isAfter(departureDateTime) &&
