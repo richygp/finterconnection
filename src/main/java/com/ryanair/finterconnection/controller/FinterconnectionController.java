a9ac096b0c724b78183b00443cc033646769adc5
ricardo <richygp@gmail.com>
2023-01-13 15:07:28 +0100

    Appends the DTOs for the interconnection response and adds controller log

--- src/main/java/com/ryanair/finterconnection/controller/FinterconnectionController.java
+++ src/main/java/com/ryanair/finterconnection/controller/FinterconnectionController.java
@@ -1,21 +1,29 @@
 package com.ryanair.finterconnection.controller;
 
+import com.ryanair.finterconnection.dto.InterconnectionDTO;
 import com.ryanair.finterconnection.dto.RouteDTO;
 import com.ryanair.finterconnection.dto.ScheduleDTO;
 import com.ryanair.finterconnection.apiclients.ryanairroutesclient.IRyanairRoutesApiClient;
 import com.ryanair.finterconnection.apiclients.ryanairscheduleclient.IRyanairScheduleApiClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class FinterconnectionController {
+    private static final Logger logger = Logger.getLogger(FinterconnectionController.class.getName());
     private final IRyanairRoutesApiClient ryanairRoutesApiClient;
     private final IRyanairScheduleApiClient ryanairScheduleApiClient;
 
     public FinterconnectionController(
             IRyanairRoutesApiClient ryanairRoutesApiClient, IRyanairScheduleApiClient ryanairScheduleApiClient) {
+        logger.log(Level.INFO, "Starting up the Ryanair Flight Interconnection Controller...");
         this.ryanairRoutesApiClient = ryanairRoutesApiClient;
         this.ryanairScheduleApiClient = ryanairScheduleApiClient;
     }
@@ -28,5 +36,11 @@ public class FinterconnectionController {
     @GetMapping("/schedules")
     public ScheduleDTO getSchedule() {
         return ryanairScheduleApiClient.getScheduleForYearAndMonth("DUB", "WRO", "2023", "6");
+    }
+
+    @GetMapping("/interconnections")
+    public List<InterconnectionDTO> getInterconnections(@RequestParam Map<String,String> paramsAsMap) {
+        logger.log(Level.INFO, "Parameters are {0}", paramsAsMap.entrySet());
+        return new ArrayList<>();
     }
 }
---
+++ src/main/java/com/ryanair/finterconnection/dto/LegDTO.java
@@ -0,0 +1,4 @@
+package com.ryanair.finterconnection.dto;
+
+public record LegDTO(String departureAirport, String arrivalAirport, String departureDataTime, String arrivalDateTime) {
+}
